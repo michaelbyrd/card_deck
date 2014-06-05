42c8ce4d632625e72dd856c619942c2eca737432
Michael Byrd <michaelbyrdsemail@gmail.com>
2014-06-05 17:17:05 -0400

    the test are working, added pry debug

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,28 @@
 # class Card: Card is used in class Deck
+require 'pry'
+# n next
+# s step
+# f finish
+# c continue
+
 class Card
   attr_reader :rank, :suit
   attr_writer :rank, :suit
-
   def initialize( rank, suit)
     @rank = rank
+
     @suit = suit
 
     @hash = Hash.new { | hash, key| key }
+
     h = { A: 1, J: 11, Q: 12, K: 13 }
+
     @hash.merge!(h)
+
   end # initialize
 
   def rank_hash
+    # binding.pry
     if @hash.keys.include?(@rank)
       @hash[@rank]
     end # if
@@ -20,7 +30,9 @@ class Card
   end # rank
 
   def greater_than?(card)
-    rank > card.rank_hash
+    p rank_hash
+    p card.rank_has
+    rank_hash > card.rank_hash
   end # greater_than?
 end # class Card
 
--- test/card_test.rb
+++ test/card_test.rb
@@ -13,21 +13,21 @@ class CardTest < MiniTest::Unit::TestCase
   end # test that card has rank
 
   def test_that_ace_is_low
-    ace = Card.new(1, :spades)
+    ace = Card.new(:A, :spades)
     two = Card.new(2, :hearts)
 
     assert two.greater_than?(ace)
     refute ace.greater_than?(two)
   end # test that ace is low
 
-  # def test_face_card_ranks
-  #   ten = Card.new(10, :hearts)
-  #   jack = Card.new(:J, :spades)
-  #   queen = Card.new(:Q, :diamonds)
-  #   king = Card.new(:K, :clubs)
-  #
-  #   assert king.greater_than?(queen)
-  #   assert queen.greater_than?(jack)
-  #   assert jack.greater_than?(ten)
-  # end
+  def test_face_card_ranks
+    ten = Card.new(10, :hearts)
+    jack = Card.new(:J, :spades)
+    queen = Card.new(:Q, :diamonds)
+    king = Card.new(:K, :clubs)
+
+    assert king.greater_than?(queen)
+    assert queen.greater_than?(jack)
+    assert jack.greater_than?(ten)
+  end
 end
