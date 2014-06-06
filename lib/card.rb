3ac4f408348058544d56bceb94048cc9f0e95d9a
Michael Byrd <michaelbyrdsemail@gmail.com>
2014-06-05 22:27:39 -0400

    issues with the deck in order test

--- lib/card.rb
+++ lib/card.rb
@@ -8,32 +8,28 @@ require 'pry'
 class Card
   attr_reader :rank, :suit
   attr_writer :rank, :suit
-  def initialize( rank, suit)
+  def initialize(rank, suit)
     @rank = rank
-
     @suit = suit
-
-    @hash = Hash.new { | hash, key| key }
-
-    h = { A: 1, J: 11, Q: 12, K: 13 }
-
-    @hash.merge!(h)
-
+    @hash = nil
+    run
   end # initialize
 
-  def rank_hash
-    # binding.pry
-    if @hash.keys.include?(@rank)
-      @hash[@rank]
-    end # if
-    @rank
+  def run
+    make_hash
+  end # run
+
+  def make_hash
+    @hash = Hash.new { | hash, key| key }
+    h = { A: 1, J: 11, Q: 12, K: 13 }
+    @hash.merge!(h)
+  end # make_hash
+
+  def rank_value
+    @hash[@rank]
   end # rank
 
   def greater_than?(card)
-    p rank_hash
-    p card.rank_has
-    rank_hash > card.rank_hash
+    rank_value > card.rank_value
   end # greater_than?
 end # class Card
-
-# Card.new( 10, :diamonds)
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,50 @@
 require "card"
 # ruby -Ilib lib/deck.rb
 class Deck
+  attr_reader :deck
+  attr_writer :deck
+
   def initialize
+    @values = nil
+    @suits = nil
     @deck = []
-    @count = 0
+    run
   end # initialize
 
+  def run
+    make_arrays
+    make_deck
+  end # run
+
+  def make_arrays
+    @values =
+    [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+    @suits = [:clubs, :diamonds, :hearts, :spades]
+  end # make_arrays
+
   def make_deck
-    while (count < 52)
-      @deck << Card.new(2, :spades)
+    count = 0
+    while count < 52
+      v = count % 13
+      s = (count / 13 % 4)
+      deck << Card.new(@values[v], @suits[s])
       count += 1
     end # while
   end # make_deck
+
+  def draw
+    if deck.size != 0
+      deck.shift
+    else
+      nil
+    end # if
+  end # draw
+
+  def size
+    deck.size
+  end # size
+
+  def cards_left
+    size
+  end # cards_left
 end # class card
